3a3028c220dffeb6e09b8ea451e3444016746826
Madiha-Shah567 <159524169+Madiha-Shah567@users.noreply.github.com>
2024-05-14 14:07:36 +0500

    commit

---
+++ class1.ts
@@ -0,0 +1,23 @@
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+console.log(chalk.yellow.bold("\n\t\t\t\t<======="), chalk.bgMagenta.yellow.italic.bold("HAPPY MOTHER'S DAY!"), chalk.yellow.bold("=======>"));
+console.log(chalk.yellow.bold("\t\t  <*"), chalk.bgWhite.bgMagenta.italic.bold("MOM, THE BEAUTIFUL INTERPRETER OF THE LANGUAGE OF THE HEART!"), chalk.yellow.bold("*>\n"));
+console.log(chalk.yellow.bold("\n\t\t\t\t<-*-"), chalk.bgMagenta.yellow.italic.bold("I LOVE YOU MAMA!"), chalk.yellow.bold("-*->"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
